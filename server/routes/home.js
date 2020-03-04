85a0b1a26b29134e144196d6e01e87738a018b29
ed <ed@maidavale.org>
2020-03-04 10:57:32 +0000

    Add table of interventions

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,7 +1,17 @@
+const dataServiceBuilder = require('../services/dataService')
+
 module.exports = function Home() {
   return {
     index: async (req, res) => {
-      res.render('pages/index', {})
+      const client = dataServiceBuilder(res.locals.user.token)
+      const response = await client.getInterventions()
+
+      const displayInterventions = response.interventions.map(intervention => [
+        { text: intervention.id },
+        { text: intervention.name },
+      ])
+
+      res.render('pages/index', { interventions: displayInterventions })
     },
   }
 }
